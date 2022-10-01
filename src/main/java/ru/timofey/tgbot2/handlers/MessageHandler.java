8ecdd5f50ff1783ed99f4678b136986142efa0f5
Timofey <tim.panov1337@gmail.com>
2022-10-01 14:12:13 +0300

    merged hhParser branch

--- src/main/java/ru/timofey/tgbot2/handlers/MessageHandler.java
+++ src/main/java/ru/timofey/tgbot2/handlers/MessageHandler.java
@@ -1,6 +1,7 @@
 package ru.timofey.tgbot2.handlers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
